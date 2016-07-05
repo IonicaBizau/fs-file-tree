3ad223302d9ab4682b645c6d7b4a58674da1a821
Ionică Bizău <bizauionica@gmail.com>
2016-07-05 13:37:37 +0300

    Examples

--- example/index.js
+++ example/index.js
@@ -2,4 +2,36 @@
 
 const fsFileTree = require("../lib");
 
-console.log(fsFileTree());
+// File tree looks like this:
+// .
+// ├── a
+// │   ├── b
+// │   │   ├── c
+// │   │   └── test.js
+// │   └── two\ words
+// └── index.js
+
+// The tree object looks like this:
+fsFileTree(__dirname, (err, tree) => {
+    console.log(tree);
+    // { a:
+    //    { b: { c: {}, 'test.js': [Object] },
+    //      'two words':
+    //       { path: '.../example/a/two words',
+    //         stat: [Object] } },
+    //   'index.js':
+    //    { path: '.../example/index.js',
+    //      stat:
+    //       {...} } }
+});
+
+console.log(fsFileTree.sync(__dirname, { camelCase: true }));
+// { a:
+//    { b: { c: {}, testJs: [Object] },
+//      twoWords:
+//       { path: '.../example/a/two words',
+//         stat: [Object] } },
+//   indexJs:
+//    { path: '.../example/index.js',
+//      stat:
+//       {...} } }
